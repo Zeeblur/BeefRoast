581149628a6206816a9c971fa0840837657226af
hamcam98 <hamcalum10@gmail.com>
2018-01-27 15:16:33 +0000

    Merge pull request #3 from Zoeoeh/calum
    Calum

---
+++ BeefRoast/Assets/Scripts/Push.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    public float Speed = 6.0f;
+
+    Vector3 pushDir;
+    public Animator anim;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Called if player is adjacent to a block and 
+    //  A key is being held to push the block
+    public void MoveBlock(Vector3 playerMove)
+    {
+        pushDir = new Vector3(playerMove.x, 0, playerMove.z);
+
+        
+    }
+
+}
