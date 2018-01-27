dfb12b68cd75e47b14296ba1dd95cc69cee09a49
Zoe Wall <zoe@zwall.co.uk>
2018-01-27 23:50:37 +0000

    Changing colour properly and position. need to update length

--- BeefRoast/Assets/Scripts/Laser.cs
+++ BeefRoast/Assets/Scripts/Laser.cs
@@ -57,22 +57,29 @@ public class Laser : MonoBehaviour
         //beamCol.SetColor("_Color", color);
         meshes = laser.GetComponentsInChildren<MeshRenderer>();
 
+        ChangeColour(color);
+
+    }
+	
+    void ChangeColour(colours c)
+    {
         // change colour 
         foreach (MeshRenderer m in meshes)
         {
-            m.material = colouredMat[(int)color];
+            m.material = colouredMat[(int)c];
         }
 
-        Enum.GetName(typeof(colours), color);
+        Enum.GetName(typeof(colours), c);
 
         // change light col
-        pointLight.GetComponent<Light>().color = shade[(int)color];
-        emitLight.GetComponent<Light>().color = shade[(int)color];
+        pointLight.GetComponent<Light>().color = shade[(int)c];
+        emitLight.GetComponent<Light>().color = shade[(int)c];
     }
-	
+
 	// Update is called once per frame
 	void Update ()
     {
+        ChangeColour(color);
         // change size per raycast
         laserScale = new Vector3(thickness, thickness, 10 * length);
         laser.transform.localScale = laserScale;
@@ -106,17 +113,27 @@ public class Laser : MonoBehaviour
                     // get colour from spawner
                     for(int i =0; i < shade.Length; i++)
                     {
-                        if (target.GetComponent<MeshRenderer>().material.color == shade[i])
+                        if (target.GetComponent<MeshRenderer>().material.color.r == shade[i].r &&
+                            target.GetComponent<MeshRenderer>().material.color.g == shade[i].g &&
+                            target.GetComponent<MeshRenderer>().material.color.b == shade[i].b)
                         {
+                            Debug.Log("UASS");
                             chosenColour = i;
-                            return;
+                            break;
                         }
                     }
 
                     GameObject colouredBeam = Instantiate(emitPrefab) as GameObject;
                     colouredBeam.GetComponent<Laser>().color = (colours)chosenColour;
+                    colouredBeam.GetComponent<Laser>().emitPrefab = emitPrefab;
                     colouredBeam.transform.SetParent(spawner, false);
                 }
+
+                spawner.position = pointLight.transform.position;
+
+                spawner.localPosition = new Vector3(spawner.localPosition.x,
+                    spawner.localPosition.y,
+                    -spawner.localPosition.z - 0.1f);
             }
         }
 
