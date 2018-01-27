33d94a7507ffece16c9d8ca7b802c5853a758ad3
hamcam98 <hamcalum10@gmail.com>
2018-01-27 15:22:33 +0000

    Push Update

--- BeefRoast/Assets/Scripts/Push.cs
+++ BeefRoast/Assets/Scripts/Push.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class Push : MonoBehaviour
 {
-    public float Speed = 6.0f;
+    public float Speed = 1.0f;
 
-<<<<<<< HEAD
     Vector3 pushDir;
-=======
-    Vector3 moveBlock;
->>>>>>> 6407a181a86c6e5538e9da1fb9cea8331a7b9587
     public Animator anim;
     Rigidbody rb;
 
@@ -23,14 +19,7 @@ public class Push : MonoBehaviour
     //  A key is being held to push the block
     public void MoveBlock(Vector3 playerMove)
     {
-<<<<<<< HEAD
         pushDir = new Vector3(playerMove.x, 0, playerMove.z);
-
-        
-=======
-            moveBlock.x = playerMove.x;
-            moveBlock.z = playerMove.z;
->>>>>>> 6407a181a86c6e5538e9da1fb9cea8331a7b9587
     }
 
 }
