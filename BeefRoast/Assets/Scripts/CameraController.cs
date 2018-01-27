c28e5129cec18de651d6c535afc5b6856e130512
Zoe Wall <zoe@zwall.co.uk>
2018-01-27 13:46:44 +0000

    camera work

---
+++ BeefRoast/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Quaternion rotation;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // initial rot
+        rotation = transform.rotation;
+
+        offset = transform.localPosition;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.rotation = rotation;
+        transform.position = transform.parent.position + offset;
+	}
+}
